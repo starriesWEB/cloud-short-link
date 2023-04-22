574bb8cb11ffe35332e0b914802f3f476458be17
starriesWEB <starries@aliyun.com>
2023-04-22 22:32:33 +0800

    account 配置文件

---
+++ cloud-account/src/main/java/com/starry/AccountApplication.java
@@ -0,0 +1,22 @@
+package com.starry;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@MapperScan("com.starry.mapper")
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
