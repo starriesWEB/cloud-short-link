e728001af365946b4533d06ac543f0962c44fb58
starriesWEB <starries@aliyun.com>
2023-04-22 12:35:01 +0800

    统一返回对象

---
+++ cloud-common/src/main/java/com/starry/exception/CustomExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.starry.exception;
+
+
+import com.starry.utils.JsonData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+//@RestControllerAdvice
+@Slf4j
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public JsonData handler(Exception e) {
+
+        if (e instanceof BizException) {
+            BizException bizException = (BizException) e;
+            log.error("[业务异常]{}", e);
+            return JsonData.buildCodeAndMsg(bizException.getCode(), bizException.getMsg());
+        } else {
+            log.error("[系统异常]{}", e);
+            return JsonData.buildError("系统异常");
+        }
+
+    }
+
+}
