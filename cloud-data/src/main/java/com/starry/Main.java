cf5849288f356e013f4524cac8e11b9945afb442
starriesWEB <starries@aliyun.com>
2023-04-22 12:02:25 +0800

    init

---
+++ cloud-data/src/main/java/com/starry/Main.java
@@ -0,0 +1,13 @@
+package com.starry;
+
+/**
+ * @author starry
+ * @version 1.0
+ * @date ${DATE} ${TIME}
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
