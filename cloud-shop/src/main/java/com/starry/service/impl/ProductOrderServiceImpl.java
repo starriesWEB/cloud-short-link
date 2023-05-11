b5ffe0e41c911077be3b1305a250d0d315b25d8f
starriesWEB <starries@aliyun.com>
2023-05-11 17:39:55 +0800

    订单关闭前查询三方支付状态

--- cloud-shop/src/main/java/com/starry/service/impl/ProductOrderServiceImpl.java
+++ cloud-shop/src/main/java/com/starry/service/impl/ProductOrderServiceImpl.java
@@ -163,8 +163,7 @@ public class ProductOrderServiceImpl extends ServiceImpl<ProductOrderMapper, Pro
             payInfoVO.setOutTradeNo(outTradeNo);
             payInfoVO.setAccountNo(accountNo);
 
-            //TODO 需要向第三方支付平台查询状态
-            String payResult = "";
+            String payResult = payFactory.queryPayStatus(payInfoVO);
 
             if(StringUtils.isBlank(payResult)){
                 //如果为空，则未支付成功，本地取消订单
@@ -174,7 +173,28 @@ public class ProductOrderServiceImpl extends ServiceImpl<ProductOrderMapper, Pro
                 //支付成功，主动把订单状态更新成支付
                 log.warn("支付成功，但是微信回调通知失败，需要排查问题:{}",eventMessage);
                 productOrderManager.updateOrderPayState(outTradeNo,accountNo,ProductOrderStateEnum.PAY.name(),ProductOrderStateEnum.NEW.name());
-                //触发支付成功后的逻辑， TODO
+                //触发支付成功后的逻辑
+                Map<String, Object> content = new HashMap<>(4);
+                content.put("outTradeNo", outTradeNo);
+                content.put("buyNum", productOrderDO.getBuyNum());
+                content.put("accountNo", accountNo);
+                content.put("product", productOrderDO.getProductSnapshot());
+
+                //构建消息
+                EventMessage payEventMessage = EventMessage.builder()
+                        .bizId(outTradeNo)
+                        .accountNo(accountNo)
+                        .messageId(outTradeNo)
+                        .content(JsonUtil.obj2Json(content))
+                        .eventMessageType(EventMessageType.PRODUCT_ORDER_PAY.name())
+                        .build();
+                //如果key不存在，则设置成功，返回true
+                Boolean flag = redisTemplate.opsForValue().setIfAbsent(outTradeNo, "OK", 3, TimeUnit.DAYS);
+                if (Boolean.TRUE.equals(flag)) {
+                    rabbitTemplate.convertAndSend(rabbitMQConfig.getOrderEventExchange(),
+                            rabbitMQConfig.getOrderUpdateTrafficRoutingKey(), payEventMessage);
+                    return false;
+                }
 
             }
         }
